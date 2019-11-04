78323d18a9b4f65c3dab29dda84a6472fa1e8655
draczi <48152518+draczi@users.noreply.github.com>
2019-11-04 14:14:26 +0100

    sql file hozzaadasa

--- mvc.sql
+++ mvc.sql
@@ -1,13 +1,12 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.5
+-- version 4.9.0.1
 -- https://www.phpmyadmin.net/
 --
 -- Gép: 127.0.0.1
--- Létrehozás ideje: 2019. Aug 29. 23:20
--- Kiszolgáló verziója: 10.1.38-MariaDB
--- PHP verzió: 7.3.2
+-- Létrehozás ideje: 2019. Nov 04. 14:13
+-- Kiszolgáló verziója: 10.4.6-MariaDB
+-- PHP verzió: 7.2.22
 
-SET FOREIGN_KEY_CHECKS=0;
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
 START TRANSACTION;
@@ -31,31 +30,30 @@ USE `mvc`;
 -- Tábla szerkezet ehhez a táblához `contacts`
 --
 
-DROP TABLE IF EXISTS `contacts`;
-CREATE TABLE IF NOT EXISTS `contacts` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `fname` varchar(150) COLLATE utf8_hungarian_ci NOT NULL,
-  `lname` varchar(150) COLLATE utf8_hungarian_ci NOT NULL,
-  `email` varchar(175) COLLATE utf8_hungarian_ci NOT NULL,
-  `phone1` varchar(50) COLLATE utf8_hungarian_ci NOT NULL,
-  `phone2` varchar(50) COLLATE utf8_hungarian_ci NOT NULL,
-  `phone3` varchar(50) COLLATE utf8_hungarian_ci NOT NULL,
-  `address` varchar(255) COLLATE utf8_hungarian_ci NOT NULL,
-  `address2` varchar(255) COLLATE utf8_hungarian_ci NOT NULL,
-  `city` varchar(255) COLLATE utf8_hungarian_ci NOT NULL,
-  `state` varchar(150) COLLATE utf8_hungarian_ci NOT NULL,
-  `zip_code` varchar(50) COLLATE utf8_hungarian_ci NOT NULL,
-  `country` varchar(155) COLLATE utf8_hungarian_ci NOT NULL,
-  `user_id` int(11) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_hungarian_ci;
+CREATE TABLE `contacts` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  `fname` varchar(155) COLLATE utf8_hungarian_ci DEFAULT NULL,
+  `lname` varchar(155) COLLATE utf8_hungarian_ci DEFAULT NULL,
+  `email` varchar(155) COLLATE utf8_hungarian_ci DEFAULT NULL,
+  `address` varchar(255) COLLATE utf8_hungarian_ci DEFAULT NULL,
+  `address2` varchar(255) COLLATE utf8_hungarian_ci DEFAULT NULL,
+  `city` varchar(100) COLLATE utf8_hungarian_ci DEFAULT NULL,
+  `state` varchar(100) COLLATE utf8_hungarian_ci DEFAULT NULL,
+  `zip` varchar(65) COLLATE utf8_hungarian_ci NOT NULL,
+  `home_phone` varchar(55) COLLATE utf8_hungarian_ci NOT NULL,
+  `cell_phone` varchar(55) COLLATE utf8_hungarian_ci NOT NULL,
+  `work_phone` varchar(55) COLLATE utf8_hungarian_ci DEFAULT NULL,
+  `deleted` tinyint(1) DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_hungarian_ci;
 
 --
 -- A tábla adatainak kiíratása `contacts`
 --
 
-INSERT INTO `contacts` (`id`, `fname`, `lname`, `email`, `phone1`, `phone2`, `phone3`, `address`, `address2`, `city`, `state`, `zip_code`, `country`, `user_id`) VALUES
-(1, 'Istvan', 'Dracz', 'istvan.dracz@gmail.com', '06203407165', '', '', 'Megyeri út 226', '', 'Budapest', '', '1048', 'Magyarorszag', 0);
+INSERT INTO `contacts` (`id`, `user_id`, `fname`, `lname`, `email`, `address`, `address2`, `city`, `state`, `zip`, `home_phone`, `cell_phone`, `work_phone`, `deleted`) VALUES
+(1, 1, 'Istvan', 'Dracz', 'istvan.dracz@gmail.com', 'Megyeri ut 226', '', 'Budapest', 'Pest', '1048', '1234567', '06203407165', '123456789ö', 0),
+(9, 1, 'Zsofia', 'Dracz-Lantos', 'zsoflesh@hotmail.com', 'Megyeri &uacute;t 226', '', 'Budapest', 'Budapest', '1048', '', '204100908', NULL, 0);
 
 -- --------------------------------------------------------
 
@@ -63,27 +61,23 @@ INSERT INTO `contacts` (`id`, `fname`, `lname`, `email`, `phone1`, `phone2`, `ph
 -- Tábla szerkezet ehhez a táblához `users`
 --
 
-DROP TABLE IF EXISTS `users`;
-CREATE TABLE IF NOT EXISTS `users` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
   `username` varchar(150) COLLATE utf8_hungarian_ci NOT NULL,
   `email` varchar(150) COLLATE utf8_hungarian_ci NOT NULL,
   `password` varchar(150) COLLATE utf8_hungarian_ci NOT NULL,
   `fname` varchar(150) COLLATE utf8_hungarian_ci NOT NULL,
   `lname` varchar(150) COLLATE utf8_hungarian_ci NOT NULL,
-  `acl` text COLLATE utf8_hungarian_ci,
-  `deleted` tinyint(4) DEFAULT '0',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8 COLLATE=utf8_hungarian_ci;
+  `acl` text COLLATE utf8_hungarian_ci DEFAULT NULL,
+  `deleted` tinyint(4) DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_hungarian_ci;
 
 --
 -- A tábla adatainak kiíratása `users`
 --
 
 INSERT INTO `users` (`id`, `username`, `email`, `password`, `fname`, `lname`, `acl`, `deleted`) VALUES
-(1, 'draczi', 'istvan.dracz@gmail.com', '$2y$10$cLwJqj/naZ2oFb5px.EwLuh0JHhWPbTRo.eFERgZg/GLGS/5Z6o4O', 'Istvan', 'Dracz', '', 0),
-(3, 'kovacsb', 'kovacs@gmail.com', '$2y$10$YFBPqsY6zkieuNHQ6UiMT.BWa0j0erWmjmP1odGGtJZfsEyFsl/YC', 'B&eacute;la', 'Kov&aacute;cs', NULL, NULL),
-(4, 'nagygabor', 'asdsa@asfsaf.hu', '$2y$10$dZhc5dShTzAvtP05HmrrnO5vOpnj8FLMmvQhbIJqIzLZSwQkV56tS', 'G&eacute;bor', 'Nagy', NULL, 0);
+(1, 'draczi', 'istvan.dracz@gmail.com', '$2y$10$cLwJqj/naZ2oFb5px.EwLuh0JHhWPbTRo.eFERgZg/GLGS/5Z6o4O', 'Istvan', 'Dracz', '', 0);
 
 -- --------------------------------------------------------
 
@@ -91,15 +85,58 @@ INSERT INTO `users` (`id`, `username`, `email`, `password`, `fname`, `lname`, `a
 -- Tábla szerkezet ehhez a táblához `users_sessions`
 --
 
-DROP TABLE IF EXISTS `users_sessions`;
-CREATE TABLE IF NOT EXISTS `users_sessions` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `users_sessions` (
+  `id` int(11) NOT NULL,
   `user_id` int(11) NOT NULL,
   `session` varchar(255) COLLATE utf8_hungarian_ci NOT NULL,
-  `user_agent` varchar(255) COLLATE utf8_hungarian_ci NOT NULL,
-  PRIMARY KEY (`id`)
+  `user_agent` varchar(255) COLLATE utf8_hungarian_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_hungarian_ci;
-SET FOREIGN_KEY_CHECKS=1;
+
+--
+-- Indexek a kiírt táblákhoz
+--
+
+--
+-- A tábla indexei `contacts`
+--
+ALTER TABLE `contacts`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `deleted` (`deleted`),
+  ADD KEY `user_id` (`user_id`);
+
+--
+-- A tábla indexei `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- A tábla indexei `users_sessions`
+--
+ALTER TABLE `users_sessions`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- A kiírt táblák AUTO_INCREMENT értéke
+--
+
+--
+-- AUTO_INCREMENT a táblához `contacts`
+--
+ALTER TABLE `contacts`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+
+--
+-- AUTO_INCREMENT a táblához `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT a táblához `users_sessions`
+--
+ALTER TABLE `users_sessions`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
