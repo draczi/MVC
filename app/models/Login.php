f5eda59c72b98b56c0416b9e04c3e8e4b9b4c4db
draczi <istvan.dracz@gmail.com>
2021-10-03 13:00:45 +0200

    hell

--- app/models/Login.php
+++ app/models/Login.php
@@ -13,6 +13,7 @@
     public function validator() {
       $this->runValidation(new RequiredValidator($this, ['field'=>'username', 'msg' => 'Username is required']));
       $this->runValidation(new RequiredValidator($this, ['field'=>'password', 'msg' => 'Password is required']));
+      $this->runValidation(new RequiredValidator($this, ['field'=>'password', 'msg' => 'Password is required']));
     }
 
     public function getRememberMeChecked() {
